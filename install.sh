3448223e509363013d35b21dc39125afa4590a38
Hansie020 <36500271+Hansie020@users.noreply.github.com>
2021-01-03 13:12:07 +0100

    Install forgotten libraries

--- install.sh
+++ install.sh
@@ -3,10 +3,27 @@
 # waveshare 2.7inch e-Paper HAT V2
 # up and running for displaying the Copernicus wallet value
 
-# Install packages
+cd ~
+mkdir isitcopernicus
+cd isitcopernicus
+
+#install BCM2835 libraries
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
+tar zxvf bcm2835-1.60.tar.gz 
+cd bcm2835-1.60/
+sudo ./configure
+sudo make
+sudo make check
+sudo make install
+
+#install wiringPi libraries
+sudo apt-get install wiringpi
+
+# Install python libraries
 PACKAGES="python3-pip python3-pil python3-numpy"
 apt-get update
 apt-get upgrade -y
 apt-get install $PACKAGES -y
 sudo pip3 install RPi.GPIO
+sudo pip3 install spidev
 
