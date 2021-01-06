d23b5e078f6fe413988c17f44610260ab89398da
Hansie020 <hansgeldof@gmail.com>
2021-01-06 19:19:06 +0100

    Library changed and QR code displayed when pressing button 1

--- copernicusdisplay.py
+++ copernicusdisplay.py
@@ -4,7 +4,7 @@
 #sys.path.insert(1, "~pi/lib") # Adds lib folder in this directory to sys
 
 import logging
-import epd2in7b_V2
+import epd2in7
 from PIL import Image,ImageDraw,ImageFont
 from gpiozero import Button
 from signal import pause
@@ -14,75 +14,73 @@ import time
 import requests
 import json
 import time
+import os
 
 
 #font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
 
 def printToDisplay3lines(string1, string2, string3):
-    HBlackImage = Image.new('1', (epd2in7b_V2.EPD_HEIGHT, epd2in7b_V2.EPD_WIDTH), 0)  # 264*179
-    HRedImage = Image.new('1', (epd2in7b_V2.EPD_HEIGHT, epd2in7b_V2.EPD_WIDTH), 0)  # 264*179
+    HBlackImage = Image.new('1', (epd2in7.EPD_HEIGHT, epd2in7.EPD_WIDTH), 255)  # 264*179
 
-    draw = ImageDraw.Draw(HRedImage) # Create draw object and pass in the image layer we want to work with (HBlackImage)
+    draw = ImageDraw.Draw(HBlackImage) # Create draw object and pass in the image layer we want to work with (HBlackImage)
     font1 = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf', 20) # Create our font, passing in the font file and font size
     font2 = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf', 11) # Create our font, passing in the font file and font size
 
-#    draw.text((10, 0), string, font = font24, fill = 0)
-    draw.text((25, 29), string1, font = font1, fill = 255)
-    draw.text((25, 78), string2, font = font1, fill = 255)
-    draw.text((25, 127), string3, font = font1, fill = 255)
-    draw.text((timeXpos, timeYpos), lastRefreshTime, font = font2, fill = 255)
-    draw.text((ipXpos, ipYpos), ipv4_address, font = font2, fill = 255)
+    draw.text((25, 29), string1, font = font1, fill = 0)
+    draw.text((25, 78), string2, font = font1, fill = 0)
+    draw.text((25, 127), string3, font = font1, fill = 0)
+    draw.text((timeXpos, timeYpos), lastRefreshTime, font = font2, fill = 0)
+    draw.text((ipXpos, ipYpos), ipv4_address, font = font2, fill = 0)
 
-    epd.display(epd.getbuffer(HRedImage), epd.getbuffer(HRedImage))
+    epd.display(epd.getbuffer(HBlackImage))
 
 def printToDisplay2lines(string1, string2):
-    HBlackImage = Image.new('1', (epd2in7b_V2.EPD_HEIGHT, epd2in7b_V2.EPD_WIDTH), 0)  # 298*126
-    HRedImage = Image.new('1', (epd2in7b_V2.EPD_HEIGHT, epd2in7b_V2.EPD_WIDTH), 0)  # 298*126
+    HBlackImage = Image.new('1', (epd2in7.EPD_HEIGHT, epd2in7.EPD_WIDTH), 255)  # 264*179
 
-    draw = ImageDraw.Draw(HRedImage) # Create draw object and pass in the image layer we want to work with (HBlackImage)
+    draw = ImageDraw.Draw(HBlackImage) # Create draw object and pass in the image layer we want to work with (HBlackImage)
     font1 = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf', 20) # Create our font, passing in the font file and font size
     font2 = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf', 10) # Create our font, passing in the font file and font size
 
-    draw.text((25, 46), string1, font = font1, fill = 255)
-    draw.text((25, 112), string2, font = font1, fill = 255)
-    draw.text((timeXpos, timeYpos), lastRefreshTime, font = font2, fill = 255)
-    draw.text((ipXpos, ipYpos), ipv4_address, font = font2, fill = 255)
-
-    epd.display(epd.getbuffer(HRedImage), epd.getbuffer(HRedImage))
+    draw.text((25, 46), string1, font = font1, fill = 0)
+    draw.text((25, 112), string2, font = font1, fill = 0)
+    draw.text((timeXpos, timeYpos), lastRefreshTime, font = font2, fill = 0)
+    draw.text((ipXpos, ipYpos), ipv4_address, font = font2, fill = 0)
+    
+    epd.display(epd.getbuffer(HBlackImage))
 
 
 def handleKey1Press():
+    #Display QR code for donation
     global buttonPressed
     print('Key1 pressed')
-    printToDisplay2lines('ETH value: '+eth_amount,'USD value: $'+usd_amount)
+    HBlackImage = Image.new('1', (epd2in7.EPD_HEIGHT, epd2in7.EPD_WIDTH), 255)  # 298*126
+    draw = ImageDraw.Draw(HBlackImage) # Create draw object and pass in the image layer we want to work with (HBlackImage)
+    font2 = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf', 11) # Create our font, passing in the font file and font size
+    #load the QR code and past it on the image layer
+    newimage = Image.open('/home/pi/copernicusdisplay/copernicus_qr.bmp')
+    HBlackImage.paste(newimage, (47,-5))
+    #draw the address text
+    draw.text((ipXpos, ipYpos), '0x2842774a57B48ae2169Ef72eE96c04e71F599020', font = font2, fill = 0)
+    
+    #show the created images
+    epd.display(epd.getbuffer(HBlackImage))
+#    printToDisplay2lines('ETH value: '+eth_amount,'USD value: $'+usd_amount)
     buttonPressed = 1
 
 def handleKey2Press():
+    #Display ETH and EUR values
     global buttonPressed
     print('Key2 pressed')
     printToDisplay2lines('ETH value: '+eth_amount,'EUR value: €'+eur_amount)
     buttonPressed = 2
 
 def handleKey3Press():
+    #Display ETH, USD and EUR values
     global buttonPressed
     print('Key3 pressed')
     printToDisplay3lines('ETH value: '+eth_amount,'USD value: $'+usd_amount, 'EUR value: €'+eur_amount)
     buttonPressed = 3
-
-def getIPv4():
-    global ipv4_address
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except Exception:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    ipv4_address = IP
-
+    
 def getIPv4():
     global ipv4_address
 
@@ -103,7 +101,7 @@ def refreshValues():
     global eur_amount
     global buttonPressed
     global lastRefreshTime
-
+   
     #call the api for the Copernicus Value
     response = requests.get('http://isitcopernicus.art:3000/api/v1.0/price')
     print(response.text)
@@ -116,10 +114,11 @@ def refreshValues():
     eur_amount = txt.format(float(data[2]["amount"]))
     #Set last refresh variable
     lastRefreshTime = time.strftime("%b %d %Y %H:%M")
-
+    
     #get the ip address to show on the display for ssh or vnc connections
     getIPv4()
 
+    #redisplay the same screen content
     if (buttonPressed == 1):
         print('refresh key 1')
         handleKey1Press()
@@ -133,7 +132,7 @@ def refreshValues():
 #initialize
 logging.basicConfig(level=logging.DEBUG)
 
-epd = epd2in7b_V2.EPD() # get the  display
+epd = epd2in7.EPD() # get the  display
 epd.init()           # initialize the display
 print("Clear...")    # prints to console, not the display, for debugging
 epd.Clear()      # clear the display
@@ -158,13 +157,13 @@ key4 = Button(19) #set key4
 
 
 #main program
-key1.when_pressed = handleKey1Press
-key2.when_pressed = handleKey2Press
-key3.when_pressed = handleKey3Press
-key4.when_pressed = refreshValues
+key1.when_pressed = handleKey1Press # Display QR code
+key2.when_pressed = handleKey2Press # Display EUR and ETH value
+key3.when_pressed = handleKey3Press # Display ETH, USD and EUR value
+key4.when_pressed = refreshValues   # Refresh the value and redisplay the 
 
-#sleep 30 secs before starting
-time.sleep(30)
+#sleep 30 secs before starting to wait for network connectivity
+#time.sleep(30)
 
 refreshValues()
 
